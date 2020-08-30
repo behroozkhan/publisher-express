e9c9680f86024fcc8998b84faa7a1e7d4fc77673
unknown <behrooz.arabzade@gmail.com>
2020-08-30 11:16:53 +0430

    debigging new website

--- routes/website.js
+++ routes/website.js
@@ -119,7 +119,7 @@ router.post('/', async (req, res) => {
             transaction
         });
 
-        console.log("website", website.prototype);
+        console.log("website", models.Website.Instance.prototype);
         await website.addPlan(plan, {through: {boughtDate, expireDate}, transaction});
 
         await user.addWebsite(website, {transaction});
