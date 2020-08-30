9e8ce6d84b8d5b47748cc325ddc8a50e055c3fe0
unknown <behrooz.arabzade@gmail.com>
2020-08-30 11:40:32 +0430

    debigging new website

--- routes/website.js
+++ routes/website.js
@@ -116,7 +116,7 @@ router.post('/', async (req, res) => {
             subDomain,
             description
         }, {
-            include: [models.Plan],
+            include: {model: models.Plan},
             transaction
         });
 
